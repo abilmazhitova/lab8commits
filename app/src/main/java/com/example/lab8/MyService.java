dec7b878f8d7effe061ad507e16d86451e0505a4
abilmazhitova <a_abilmazhitova@mail.ru>
2025-04-08 08:05:27 +0500

    MyService.java (ForegroundService)

--- app/src/main/java/com/example/lab8/MyService.java
+++ app/src/main/java/com/example/lab8/MyService.java
@@ -1,4 +1,59 @@
 package com.example.lab8;
 
-public class MyService {
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class MyService extends Service {
+    private MediaPlayer soundPlayer;
+    private final String CHANNEL_ID = "channelId";
+
+    @Override
+    public void onCreate() {
+        soundPlayer = MediaPlayer.create(this, R.raw.song);
+        soundPlayer.setLooping(false);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        createNotificationChannel();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_music)
+                .setContentTitle("My Music Player")
+                .setContentText("Music is playing")
+                .build();
+
+        startForeground(1, notification);
+        soundPlayer.start();
+        return START_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID, "Service Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        soundPlayer.stop();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
