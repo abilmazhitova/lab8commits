2d4c98d0a396a90e2ffae41b70f7c025101ce6e9
abilmazhitova <a_abilmazhitova@mail.ru>
2025-04-08 08:06:37 +0500

    added MainActivity2.java

--- app/src/main/java/com/example/lab8/MainActivity2.java
+++ app/src/main/java/com/example/lab8/MainActivity2.java
@@ -1,4 +1,13 @@
 package com.example.lab8;
 
-public class MainActivity2 {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
 }
