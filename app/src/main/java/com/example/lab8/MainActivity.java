a887d2280e6753ace2f7cf6abfda1e636e907d36
abilmazhitova <a_abilmazhitova@mail.ru>
2025-04-08 07:57:45 +0500

    MainActivity.java (BackgroundService)

--- app/src/main/java/com/example/lab8/MainActivity.java
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
